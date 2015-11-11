a451bc6d402456116e9dd05283131be6bec72fb1
kazukgw <kazukgw@gmail.com>
2015-11-11 23:25:05 +0900

    Imprement PreCompile

--- template_loader.go
+++ template_loader.go
@@ -10,12 +10,40 @@ type TemplateLoader struct {
 	TemplateDir string
 	Common      string
 	GlobPattern string
+	PreCompile  bool
 	template.FuncMap
-	Templates map[string]*template.Template
+	Templates        map[string]*template.Template
+	TemplateBuilders map[string]*TemplateBuilder
+}
+
+type TemplateBuilder struct {
+	Paths []string
+	template.FuncMap
+}
+
+func (tb *TemplateBuilder) Build() *template.Template {
+	t := template.New("")
+	t = t.Funcs(tb.FuncMap)
+	for _, p := range tb.Paths {
+		tmpl, err := t.ParseGlob(p)
+		if err != nil {
+			panic(err.Error())
+		}
+		t = tmpl
+	}
+	return t
+}
+
+func (tl *TemplateLoader) Get(name string) *template.Template {
+	if tl.PreCompile {
+		return tl.Templates[name]
+	}
+	return tl.TemplateBuilders[name].Build()
 }
 
 func (tl *TemplateLoader) Load() {
 	tl.Templates = map[string]*template.Template{}
+	tl.TemplateBuilders = map[string]*TemplateBuilder{}
 	dir := tl.TemplateDir
 	infos, err := ioutil.ReadDir(tl.TemplateDir)
 	if err != nil {
@@ -24,16 +52,18 @@ func (tl *TemplateLoader) Load() {
 	commonp := fp.Join(dir, tl.Common)
 	for _, info := range infos {
 		if info.IsDir() && info.Name() != tl.Common {
-			t, err := template.New("").Funcs(tl.FuncMap).ParseGlob(fp.Join(commonp, tl.GlobPattern))
-			if err != nil {
-				panic(err.Error())
+			tb := &TemplateBuilder{
+				FuncMap: tl.FuncMap,
+				Paths: []string{
+					fp.Join(commonp, tl.GlobPattern),
+					fp.Join(dir, info.Name(), tl.GlobPattern),
+				},
 			}
-			p := fp.Join(dir, info.Name())
-			t, err = t.ParseGlob(fp.Join(p, tl.GlobPattern))
-			if err != nil {
-				panic(err.Error())
+			if tl.PreCompile {
+				tl.Templates[info.Name()] = tb.Build()
+			} else {
+				tl.TemplateBuilders[info.Name()] = tb
 			}
-			tl.Templates[info.Name()] = template.Must(t, nil)
 		}
 	}
 }
